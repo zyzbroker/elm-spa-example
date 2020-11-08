5f649864937fc8979394936ff3a3b6a9f9a36b03
david.z <david.z@spinsci.com>
2020-11-07 19:02:02 -0600

    refactore bootstrap codes into index.js file

---
+++ index.js
@@ -0,0 +1,28 @@
+import { Elm } from "./src/Main.elm"
+
+let storageKey = "store";
+let flags = localStorage.getItem(storageKey);
+
+window.onload = (args) => {
+  
+  let app = Elm.Main.init({flags: flags});
+  app.ports.storeCache.subscribe(function(val) {
+
+    if (val === null) {
+      localStorage.removeItem(storageKey);
+    } else {
+      localStorage.setItem(storageKey, JSON.stringify(val));
+    }
+
+    // Report that the new session was stored successfully.
+    setTimeout(function() { app.ports.onStoreChange.send(val); }, 0);
+  });
+
+  // Whenever localStorage changes in another tab, report it if necessary.
+  window.addEventListener("storage", function(event) {
+    if (event.storageArea === localStorage && event.key === storageKey) {
+      app.ports.onStoreChange.send(event.newValue);
+    }
+  }, false);
+}
+
